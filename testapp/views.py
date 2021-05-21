74a55c0e33613a147afff34e2838cc79567b100e
sivaram <sivaramk1411@gmail.com>
2021-05-21 11:51:40 +0530

    retrieve the all records from emploree table

--- testapp/views.py
+++ testapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from testapp.models import Employee
 # Create your views here.
+def retrieve_view(request):
+    employees=Employee.objects.all()
+    return render(request,'testapp/index.html',{"employees":employees})
