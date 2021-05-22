06b44ea6ef4014ce8aad66edd20e6a539cb7d9de
sivaram <sivaramk1411@gmail.com>
2021-05-22 16:27:56 +0530

    curd function based view

--- testapp/views.py
+++ testapp/views.py
@@ -13,3 +13,15 @@ def create_view(request):
           form.save()
           return redirect('/')
     return render(request,"testapp/create.html",{"form":form})
+def delete_view(request,id):
+    employee=Employee.objects.get(id=id)
+    employee.delete()
+    return redirect('/')
+def update_view(request,id):
+    employee=Employee.objects.get(id=id)
+    if request.method=='POST':
+       form=EmployeeForm(request.POST,instance=employee)
+       if form.is_valid():
+          form.save()
+          return redirect('/')
+    return render(request,"testapp/update.html",{"employee":employee})
